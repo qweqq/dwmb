2cc98f573c5cff1bed5619b9c71286e42d067a8e
do <dageneva@gmail.com>
2015-12-19 18:48:02 +0200

    progress

--- server_code/dwmb/test.py
+++ server_code/dwmb/test.py
@@ -14,9 +14,9 @@ class Blah(unittest.TestCase):
 		pass
 
 	def setUp(self):
-		self.send_request('/dump', {"yes": True})
+		#self.send_request('/dump', {"yes": True})
 		self.send_request('/drop', {"yes": True})
-		self.send_request('/dump', {"yes": True})
+		#self.send_request('/dump', {"yes": True})
 
 	def tearDown(self):
 		pass
@@ -27,99 +27,115 @@ class Blah(unittest.TestCase):
 		return data
 
 	def send_request(self, url, data):
-		full_url = "http://localhost:1234/" + str(url)
+		full_url = "http://localhost:12345/" + str(url)
 		req = urllib2.Request(full_url, self.prepare_data(data))
 		response = urllib2.urlopen(req)
 		the_page = response.read()
 		return the_page
 
-	def test_gosho(self):
-
-		user = self.send_request('/poop', {
-			'rfid' : '123'
+	def send_default_poop(self, rfid='123'):
+		result = self.send_request('/poop', {
+			'rfid' : rfid
 		})
-		user = json.loads(user)
-		print(user)
-		code = user["code"]
+		decoded = json.loads(result)
+		code = ""
+		if ("code" in decoded):
+			code = decoded["code"]
 
-		the_page = self.send_request('/register', {
+		print("send_default_poop: result: ", result)
+
+		return result, code
+
+	def send_default_registration(self, code):
+		result = self.send_request('/register', {
 			'username' : 'ivankadraganova',
 			'password' : 'password',
 			'email' : 'ivanka@abv.bg',
 			'code' : code
 		})
 
-		print("response: " + str(the_page))
+		print("send_default_registration: result: ", result)
 
-	def test_poop_alive(self):
-		result = self.send_request('/poop', {
-			'rfid' : '123'
+		return result
+
+	def send_default_alive(self, slots=[0, 0, 0, 0, 0, 0, 0, 0]):
+		result = self.send_request('/alive', {
+			'slots': slots,
+			'key': "6x9=42"
 		})
 
+		print("send_default_alive: result: ", result)
+
+		return result
+
+	def test_poop_returns_code(self):
+		result, code = self.send_default_poop()
 		result = json.loads(result)
-		print(result)
+
 		code = result["code"]
 		self.assertTrue("code" in result)
 
-		result = self.send_request('/alive', {
-			'slots': [
-				1, 0, 0, 0, 0, 0, 0, 0
-			],
-			'key': "6x9=42"
-		})
+	def test_registration(self):
+		result, code = self.send_default_poop()
+
+		result = self.send_default_registration(code)
+
+		self.assertEqual(result, '{"status":"ok","message":"registered"}')
+
+	def test_poop_alive1(self):
+		result, code = self.send_default_poop()
+
+		result = self.send_default_alive([1, 0, 0, 0, 0, 0, 0, 0])
+
 		self.assertEqual(result, '{"status":"ok","message":"connected"}')
 
-	def test_poop_register_alive(self):
-		result = self.send_request('/poop', {
-			'rfid' : '123'
-		})
+	def test_poop_register_alive1(self):
+		result, code = self.send_default_poop()
 
-		result = json.loads(result)
-		print(result)
-		code = result["code"]
-		self.assertTrue("code" in result)
+		result = self.send_default_registration(code);
 
-		the_page = self.send_request('/register', {
-			'username' : 'ivankadraganova',
-			'password' : 'password',
-			'email' : 'ivanka@abv.bg',
-			'code' : code
-		})
+		result = self.send_default_alive([1, 0, 0, 0, 0, 0, 0, 0])
 
-		result = self.send_request('/alive', {
-			'slots': [
-				1, 0, 0, 0, 0, 0, 0, 0
-			],
-			'key': "6x9=42"
-		})
 		self.assertEqual(result, '{"status":"ok","message":"connected"}')
 
 	def test_theft(self):
+		result, code = self.send_default_poop()
 
-		result = self.send_request('/poop', {
-			'rfid' : '123'
-		})
+		result = self.send_default_alive([1, 0, 0, 0, 0, 0, 0, 0])
 
-		result = json.loads(result)
-		print(result)
-		code = result["code"]
-		self.assertTrue("code" in result)
+		result = self.send_default_alive([0, 0, 0, 0, 0, 0, 0, 0])
 
-		result = self.send_request('/alive', {
-			'slots': [
-				1, 0, 0, 0, 0, 0, 0, 0
-			],
-			'key': "6x9=42"
-		})
-		self.assertEqual(result, '{"status":"ok","message":"connected"}')
-
-		result = self.send_request('/alive', {
-			'slots': [
-				0, 0, 0, 0, 0, 0, 0, 0
-			],
-			'key': "6x9=42"
-		})
 		self.assertEqual(result, '{"status":"error","message":"theft"}')
+
+	def test_poop_alive1_poop_alive0(self):
+		result, code = self.send_default_poop()
+
+		result = self.send_default_alive([1, 0, 0, 0, 0, 0, 0, 0])
+
+		result, code = self.send_default_poop()
+
+		result = self.send_default_alive([0, 0, 0, 0, 0, 0, 0, 0])
+
+		self.assertEqual(result, '{"status":"ok","message":"disconnected"}')
+
+	def test_poop_register_alive1_poop_alive0(self):
+		result, code = self.send_default_poop()
+
+		result = self.send_default_registration(code)
+
+		result = self.send_default_alive([1, 0, 0, 0, 0, 0, 0, 0])
+
+		result, code = self.send_default_poop()
+
+		result = self.send_default_alive([0, 0, 0, 0, 0, 0, 0, 0])
+
+		self.assertEqual(result, '{"status":"ok","message":"disconnected"}')
+
+	def test_poop_poop(self):
+		result, code = self.send_default_poop()
+
+		result, code = self.send_default_poop()
+		self.assertEqual(code, "")
 
 	def test_drop(self):
 		pass
