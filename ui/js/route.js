464ff2bfdf978356f1a91c68e4b70e478c0be43f
velikova <velikova@ifao.net>
2015-12-19 13:14:30 +0200

    img folder + initial js

--- ui/js/route.js
+++ ui/js/route.js
@@ -1,3 +1,14 @@
-(function () {
-  
-})();
+( function () {
+
+  console.log('bar');
+
+  $.ajax({
+    url: "../main-header.html",
+    context: document.body
+  }).done( function( data ) {
+    $( '.menu' ).append( data );
+
+    console.log('foo');
+  });
+
+} )();
