527b49267f855f0e9784c19df6ed5670d8a9767e
Angel Angelov <hextwoa@gmail.com>
2015-12-19 12:19:38 +0200

    implement communication between avr and the go code

--- pi_code/src/dwmb/test.go
+++ pi_code/src/dwmb/test.go
@@ -1,39 +1,34 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/tarm/serial"
+	"dwmb/comm"
 )
 
 func main() {
-	s, err := serial.OpenPort(&serial.Config{Name: "/dev/ttyAMA0", Baud: 115200})
+	states, _, err := comm.Init("/dev/ttyAMA0", 115200)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	n, err := s.Write([]byte("c"))
-	if err != nil {
-		log.Fatal(err)
+	state := &comm.State{Message: ""}
+	for {
+		state = <-states
+		fmt.Printf("state: %s\n", state.Message)
 	}
-
-	buf := make([]byte, 128)
-	n, err = s.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%q", buf[:n])
-
-	resp, err := http.Get("https://archlinux.org")
-	if err != nil {
-		log.Fatal(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("resp: %s\n", body)
+	return
+	/*
+		resp, err := http.Get("https://archlinux.org")
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("resp: %s\n", body)
+	*/
 }
