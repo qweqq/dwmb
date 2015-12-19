3686abf5c6b212a5c48d0f118418cc75edd61da3
velikova <velikova@ifao.net>
2015-12-19 17:21:35 +0200

    stuff

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,3 +1,16 @@
 (function () {
-  
+
+  $( '#board1 .light' ).on( 'click', function ( ev ) {
+
+    var slot = $( ev.target );
+    var slotId = slot.attr( 'data-id' );
+
+    if ( slot.hasClass( 'on' ) ) {
+      HELPERS_MODULE.switchOffSlot( slotId );
+    } else {
+      HELPERS_MODULE.switchOnSlot( slotId );
+    }
+
+  });
+
 })();
--- ui/js/util.js
+++ ui/js/util.js
@@ -1,3 +1,12 @@
 var HELPERS_MODULE = (function () {
-
+  return {
+    switchOnSlot: function ( indexOfSlot ) {
+      console.log('yiss');
+      $( "#board1 [data-id='" + indexOfSlot + "']" ).addClass( 'on' );
+    },
+    switchOffSlot: function ( indexOfSlot ) {
+      console.log('noo');
+      $( "#board1 [data-id='" + indexOfSlot + "']" ).removeClass( 'on' );
+    }
+  }
 })();
