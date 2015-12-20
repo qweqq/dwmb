a89198004a5472918c7f4f25077ee05c7343f57d
velikova <velikova@ifao.net>
2015-12-20 19:25:05 +0200

    removes minus for slots

--- ui/js/user.js
+++ ui/js/user.js
@@ -46,7 +46,7 @@
       } else if ( jsonData['status'] == 'ok' ) {
         $( '.bike-board' ).show( 'fast' );
 
-        $( '#slotNumber' ).html(-parseInt(jsonData['slot']) + 1 );
+        $( '#slotNumber' ).html( parseInt(jsonData['slot']) + 1 );
       }
     });
   }, 1000);
